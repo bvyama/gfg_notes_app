fcc09d8b67e9109c135c9ad417e1b0444c04d87a
bvyama <yamabadrivishal@gmail.com>
2025-12-11 11:48:38 +0530

    feat:Added HomePage

---
+++ src/components/NotesCard/index.jsx
@@ -0,0 +1,36 @@
+import { useNotes } from "../../context/notes-context";
+export const NotesCard = ({ id, title, text,isPinned }) => {
+    const {notesDispatch} = useNotes();
+    const onPinClick = (id) => {
+            !isPinned ?
+            notesDispatch({
+                type:'PIN',
+                payload:{id}
+            }) : notesDispatch({
+                type:'UNPIN',
+                payload:{id}
+            })
+    }
+    return (
+        <div className="w-56 border border-neutral-800 p-2 rounded-md w-[-250px]" key={id}>
+            <div className="flex justify-between border-b-2">
+                <p>{title}</p>
+                {/* The way to pass id onClick */}
+                <button onClick={()=>onPinClick(id)}>
+                    <span className={isPinned ? 'material-icons':'material-icons-outlined'}>push_pin</span>
+                </button>
+            </div>
+            <div className="flex flex-col">
+                <p>{text}</p>
+                <div className="ml-auto">
+                    <button>
+                        <span className="material-icons-outlined">archive</span>
+                    </button>
+                    <button>
+                        <span className="material-icons-outlined">delete</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
